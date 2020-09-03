3ce7821a02a4acdef2f34b214f25353e94a8c361
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-09-03 13:06:13 -0400

    Remove all typescript code

--- src/entities/module-base.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from 'discord.js';
-export interface ModuleBase {
-    name: string;
-    alias: string;
-    description: string;
-    help_text: string;
-    execute(message: Message, args: string): Promise<any>;
-}
