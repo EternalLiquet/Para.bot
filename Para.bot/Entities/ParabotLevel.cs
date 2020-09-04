550e05281202b1c086c12347d36968484d7ebaa5
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-09-04 00:46:15 -0400

    Create entities

---
+++ Para.bot/Entities/ParabotLevel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Para.bot.Entities
+{
+    public class ParabotLevel
+    {
+        
+        [BsonId]
+        public int Level { get; set; }
+        public int ExpRequirement { get; set; }
+
+        public ParabotLevel(int level, int expRequirement)
+        {
+            this.Level = level;
+            this.ExpRequirement = expRequirement;
+        }
+    }
+}
---
+++ Para.bot/Entities/ParabotUser.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Para.bot.Entities
+{
+    public class ParabotUser
+    {
+        [BsonId]
+        public string ParabotUserId { get; set; }
+        public string Username { get; set; }
+        public string ServerName { get; set; }
+        public ulong DiscordId { get; set; }
+        public ulong ServerId { get; set; }
+        public ulong CooldownDTM { get; set; }
+        public int Exp { get; set; }
+        public int Level { get; set; }
+    }
+}
