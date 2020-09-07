8650e535f5a330d535773f6ec057de2b33e7597c
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-09-07 01:37:07 -0400

    Save Settings

---
+++ Para.bot/Repository/SettingsRepository.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using Para.bot.Entities;
+using Para.bot.Util;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para.bot.Repository
+{
+    public class SettingsRepository
+    {
+        private IMongoCollection<ParabotSettings> _settingsCollection = MongoDbClient.beanDatabase.GetCollection<ParabotSettings>("settings");
+        public async Task InsertNewSettings(ParabotSettings parabotSettings)
+        {
+            try
+            {
+                await _settingsCollection.InsertOneAsync(parabotSettings);
+                Log.Information($"Settings successfully updated");
+            }
+            catch
+            {
+                Log.Error($"Error inserting settings into database");
+            }
+        }
+    }
+}
---
+++ Para.bot/Services/SettingsService.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Para.bot.Entities;
+using Para.bot.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para.bot.Services
+{
+    public class SettingsService
+    {
+        public async Task SaveGreetingSettings(SocketCommandContext context, string greetingMessage, string whereToGreet, string channelToGreet)
+        {
+            var guildChannel = context.Channel as SocketTextChannel;
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+            settings.Add("welcomeMessage", greetingMessage);
+            settings.Add("whereToGreet", whereToGreet);
+            settings.Add("channelToGreet", channelToGreet);
+            ParabotSettings newGreetingSettings = new ParabotSettings($"{guildChannel.Guild.Id}NewMemberSettings", settings);
+            SettingsRepository settingsRepo = new SettingsRepository();
+            await settingsRepo.InsertNewSettings(newGreetingSettings);
+        }
+    }
+}
