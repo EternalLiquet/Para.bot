687eddf72cb2ad4ebcf126546bcc10eaffb6ccdf
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-11-14 15:28:28 -0500

    re-add this thing i guess

--- Para.bot/Services/RoleReactService.cs
+++ Para.bot/Services/RoleReactService.cs
@@ -27,6 +27,7 @@ namespace Para.bot.Services
                 if (settings == null) return;
                 List<ParabotRoleEmotePair> roleEmoteDict = (List<ParabotRoleEmotePair>)settings.Settings["roleEmoteDict"];
                 var guild = (cachedMessage.Channel as SocketTextChannel).Guild;
+                await guild.DownloadUsersAsync();
                 Log.Debug(guild.Name);
                 Log.Debug(reaction.UserId.ToString());
                 Log.Debug(reaction.User.ToString());
